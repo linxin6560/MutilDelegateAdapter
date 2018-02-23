2cdac923590b8a80f16bab7b31e206d1dbc8f2a7
linxin <linxin@xiaoyouzi.com>
2018-02-23 15:44:40 +0800

    代码优化

--- app/src/main/java/com/levylin/mutildelegateadapter/MultiDelegateQuickAdapter.java
+++ app/src/main/java/com/levylin/mutildelegateadapter/MultiDelegateQuickAdapter.java
@@ -1,5 +1,7 @@
 package com.levylin.mutildelegateadapter;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.ViewGroup;
@@ -59,5 +61,34 @@ public class MultiDelegateQuickAdapter extends BaseMultiItemQuickAdapter<MultiIt
     public void addMultiDelegate(AMultiAdapterDelegate delegate) {
         delegates.append(delegate.getItemType(), delegate);
         addItemType(delegate.getItemType(), delegate.getLayoutId());
+    }
+
+    /**
+     * 多类型数据适配器
+     * Created by LinXin on 2017/6/16.
+     */
+    public abstract class AMultiAdapterDelegate {
+
+        protected RecyclerView.Adapter mAdapter;
+        protected Context mContext;
+
+        public AMultiAdapterDelegate(RecyclerView.Adapter adapter) {
+            this.mAdapter = adapter;
+        }
+
+        @LayoutRes
+        public abstract int getLayoutId();
+
+        public abstract int getItemType();
+
+        public abstract void convert(BaseViewHolder holder, MultiItemEntity item);
+
+        public void onCreateViewHolder(BaseViewHolder holder, int viewType) {
+
+        }
+
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            mContext = recyclerView.getContext();
+        }
     }
 }
